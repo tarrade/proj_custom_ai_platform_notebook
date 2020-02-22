9f16ced33bda4484a49c06508170ac2ac3977162
Fabien Tarrade <fabien.tarrade@gmail.com>
2020-02-22 20:56:17 +0100

    adding jupyter lab extensions

--- docker/build_image_locally.sh
+++ docker/build_image_locally.sh
@@ -3,14 +3,26 @@ cd ../
 mkdir test-docker
 pwd
 
-# copy file localy
+# copy config files localy
 cp docker/derived-pytorch-cpu/Dockerfile test-docker/.
+
 cp env/base.yml test-docker/.
-cp env/environment.yml test-docker/.
-cp env/environment_gcp.yml test-docker/.
-cp env/jupyter-notebook.yml test-docker/.
+
+cp script/bucket_gcp/scripts/.condarc test-docker/.
+cp script/bucket_gcp/scripts/pip.conf test-docker/.
+cp script/bucket_gcp/scripts/pip.conf test-docker/.
+cp script/bucket_gcp/scripts/.gitconfig test-docker/.
+
 
 cd test-docker
 
+# locally drop the line with clean up of conda packages
+sed -i.bak  '/RUN conda clean -a -y/d' Dockerfile
+
+# drop --debug option for jupyter lab when running locally
+#sed -i.bak  's/--debug//g' Dockerfile
+
 echo "build the docker image"
 docker build -f Dockerfile .
+
+cd ../docker
