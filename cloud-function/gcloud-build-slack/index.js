72bde53756c5064128086595eca2c3a3f40b49d0
Fabien Tarrade <fabien.tarrade@gmail.com>
2020-04-10 11:00:53 +0200

    updating the code with what is in the UI

--- cloud-function/gcloud-build-slack/index.js
+++ cloud-function/gcloud-build-slack/index.js
@@ -1,12 +1,9 @@
 const { IncomingWebhook } = require('@slack/webhook');
 const url = process.env.SLACK_WEBHOOK_URL;
-
 const webhook = new IncomingWebhook(url);
-
 // subscribeSlack is the main function called by Cloud Functions.
 module.exports.subscribeSlack= (pubSubEvent, context) => {
   const build = eventToBuild(pubSubEvent.data);
-
   // Skip if the current status is not in the status list.
   // Add additional statuses to list if you'd like:
   // QUEUED, WORKING, SUCCESS, FAILURE,
@@ -15,17 +12,14 @@ module.exports.subscribeSlack= (pubSubEvent, context) => {
   if (status.indexOf(build.status) === -1) {
     return;
   }
-
   // Send message to Slack.
   const message = createSlackMessage(build);
   webhook.send(message);
 };
-
 // eventToBuild transforms pubsub event message to a build object.
 const eventToBuild = (data) => {
   return JSON.parse(Buffer.from(data, 'base64').toString());
 }
-
 // createSlackMessage creates a message from a build object.
 const createSlackMessage = (build) => {
   const message = {
