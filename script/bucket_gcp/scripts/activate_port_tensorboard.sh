35a55f42bef7155a6fe5c6fc4ec67446be015835
Fabien Tarrade <fabien.tarrade@gmail.com>
2020-04-18 11:11:03 +0200

    updated scripts

--- script/bucket_gcp/scripts/activate_port_tensorboard.sh
+++ script/bucket_gcp/scripts/activate_port_tensorboard.sh
@@ -3,7 +3,7 @@
 echo "This is the start of the activate_port_tensorboard.sh script"
 
 # Adding port forwarding for TensorBoard on port 6006
-sed -e '/docker run -d/a\    -p 6006:6006 \\' /opt/deeplearning/bin/load_container.sh
+sed -i '/docker run -d/a\    -p 6006:6006 \\' /opt/deeplearning/bin/load_container.sh
 cat /opt/deeplearning/bin/load_container.sh
 
 echo "This is the end of the activate_port_tensorboard.sh script"
